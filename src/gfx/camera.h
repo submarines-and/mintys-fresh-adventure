939d55aa34c04b042f3fcac93cb24366b4dc5fe3
submarines-and <johannes.sorensen@ingka.ikea.com>
2022-12-29 17:09:22 +0100

    Camera up

--- src/gfx/camera.h
+++ src/gfx/camera.h
@@ -10,7 +10,9 @@ enum Camera_Movement {
     FORWARD,
     BACKWARD,
     LEFT,
-    RIGHT
+    RIGHT,
+    UP,
+    DOWN
 };
 
 // Default camera values
@@ -77,8 +79,13 @@ public:
         if (direction == RIGHT)
             Position += Right * velocity;
 
+        if (direction == UP)
+            Position += Up * velocity;
+        if (direction == DOWN)
+            Position -= Up * velocity;
+
         // make sure the user stays at the ground level
-        Position.y = 0.0f; // <-- this one-liner keeps the user at the ground level (xz plane)
+        //  Position.y = 0.0f; // <-- this one-liner keeps the user at the ground level (xz plane)
     }
 
     // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
