d82e3b38fbe4d4b2a686f52b96dcdf4a9a05723a
submarines-and <johannes.sorensen@ingka.ikea.com>
2023-01-04 14:52:18 +0100

    Spawn in the center

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,6 @@ Global& global = global_instance;
 
 void init(int ticks)
 {
-    global.world.generate(100, TileAtlas{
-                                   .spritePath = "assets/forest.jpg",
-                                   .atlasSize = glm::vec2(1, 1),
-                                   .tileSize = glm::vec2(1000, 1000),
-                               });
-
     global.renderer->loadShader(Shader::TILE, "shaders/tile.vert", "shaders/tile.frag");
     global.renderer->loadShader(Shader::SPRITE, "shaders/sprite.vert", "shaders/sprite.frag");
 }
@@ -41,12 +35,19 @@ void destroy(int ticks)
 
 int main()
 {
-    global.width = 32 * 40;
-    global.height = 32 * 20;
+    global.width = 1920;
+    global.height = 1080;
 
     Window window(global.width, global.height, init, update, render, destroy);
     global.renderer = new Renderer();
-    global.camera = new Camera(glm::vec3(0, 0, 0));
+
+    global.world.generate(100, TileAtlas{
+                                   .spritePath = "assets/forest.jpg",
+                                   .atlasSize = glm::vec2(1, 1),
+                                   .tileSize = glm::vec2(1000, 1000),
+                               });
+
+    global.camera = new Camera(glm::vec3(global.world.width / 2, global.world.height / 2, 0));
 
     window.loop();
 
