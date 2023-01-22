26c4d82d720a883c1df743acde5de6e79070c68d
submarines-and <johannes.sorensen@ingka.ikea.com>
2023-01-22 14:15:05 +0100

    Broken ray cast

--- src/gfx/mouse.cpp
+++ src/gfx/mouse.cpp
@@ -2,7 +2,48 @@
 #include "global.h"
 #include <glm/gtc/matrix_transform.hpp>
 
-glm::vec3 Mouse::getWorldPosition()
+bool Mouse::isUnderGround(glm::vec3 position)
+{
+    auto height = global.world->getTerrainHeight(position);
+    return position.y < height;
+}
+
+bool Mouse::intersectionInRange(float start, float end, glm::vec3 ray)
+{
+    glm::vec3 startPoint = getPointOnRay(ray, start);
+    glm::vec3 endPoint = getPointOnRay(ray, end);
+
+    return !isUnderGround(startPoint) && isUnderGround(endPoint);
+}
+
+glm::vec3 Mouse::getPointOnRay(glm::vec3 ray, float distance)
+{
+    glm::vec3 cameraPosition = global.camera->position;
+    glm::vec3 start(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+    glm::vec3 scaledRay(ray.x * distance, ray.y * distance, ray.z * distance);
+
+    return start + scaledRay;
+}
+
+glm::vec3 Mouse::binarySearch(int count, float start, float finish, glm::vec3 ray)
+{
+    float half = start + ((finish - start) / 2.0f);
+
+    // count is arbitrary
+    if (count >= 200) {
+        return getPointOnRay(ray, half);
+    }
+
+    // search
+    if (intersectionInRange(start, half, ray)) {
+        return binarySearch(count + 1, start, half, ray);
+    }
+    else {
+        return binarySearch(count + 1, half, finish, ray);
+    }
+}
+
+glm::vec3 Mouse::castRay()
 {
     // normalized device coordinates
     float normalizedX = (2.0f * position.x) / global.width - 1.0f;
@@ -21,4 +62,10 @@ glm::vec3 Mouse::getWorldPosition()
     mouseRay = glm::normalize(mouseRay);
 
     return mouseRay;
+}
+
+glm::vec3 Mouse::getWorldPosition()
+{
+    auto ray = castRay();
+    return binarySearch(0, 0, 300, ray);
 }
--- src/gfx/mouse.h
+++ src/gfx/mouse.h
@@ -6,6 +6,13 @@ public:
     glm::vec2 position;
     glm::vec2 offset;
 
+private:
+    bool isUnderGround(glm::vec3 position);
+    bool intersectionInRange(float start, float end, glm::vec3 ray);
+    glm::vec3 binarySearch(int count, float start, float finish, glm::vec3 ray);
+    glm::vec3 getPointOnRay(glm::vec3 ray, float distance);
+    glm::vec3 castRay();
+
 public:
     glm::vec3 getWorldPosition();
 };
